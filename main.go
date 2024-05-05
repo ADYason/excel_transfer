59ccec6e2c9f011e8350c0082961cc38c3e7f7a0
ADYason <goryainov.yaroslav@gmail.com>
2024-05-05 13:31:46 +0300

    version without excel

--- main.go
+++ main.go
@@ -1,95 +1,106 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path"
-
-	"github.com/xuri/excelize/v2"
 )
 
 func main() {
-	if len(os.Args) != 4 {
-		log.Print("Not enough args")
-		log.Print("Need folder to search, field to search, output folder")
-		return
-	}
 	log.Print("Start working", "\n")
-	search_dir := os.Args[1]
-	search_field := os.Args[2]
-	output_dir := os.Args[3]
-	sheet_to_search := parseSheetArg()
-	files, err := os.ReadDir(search_dir)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, file := range files {
-		src := path.Join(search_dir, file.Name())
-		dest := path.Join(output_dir, file.Name())
-		f, err := excelize.OpenFile(src)
-		if err != nil {
-			log.Printf("%s not an excel file", file.Name())
-			continue
-		}
-		defer func() {
-			if err := f.Close(); err != nil {
-				log.Println(err)
-			}
-		}()
-		rows, err := f.GetRows(sheet_to_search)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		var found_flag bool
-		found_flag = false
-		for _, row := range rows {
-			for _, colCell := range row {
-				if colCell == search_field {
-					log.Printf("%s found in %s", search_field, file.Name())
-					log.Printf("Transfering %s to %s", file.Name(), output_dir)
-					copyFileContents(src, dest)
-					found_flag = true
-					break
-				}
-			}
-		}
-		if !found_flag {
-			log.Printf("'%s' not found in %s file\n", search_field, file.Name())
-		}
+	file_to_pars, out_dir := parseArgs()
+	log.Print("Find file to parse and out dirrectory")
+	folders_to_copy, _ := getFolders(file_to_pars)
+	log.Print("Got folders to copy")
+	for _, folder := range folders_to_copy {
+		cpDir(folder, path.Join(out_dir, path.Base(folder)))
 	}
 	log.Print("End working")
 
 }
 
-func parseSheetArg() string {
-	if len(os.Args) == 5 {
-		return os.Args[4]
-	} else {
-		return "Sheet1"
+func getFolders(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
 	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
 
-func copyFileContents(src, dst string) (err error) {
-	in, err := os.Open(src)
-	if err != nil {
-		return
+func parseArgs() (string, string) {
+	if len(os.Args) != 3 {
+		panic("Not enough args. Need file to parse (.txt), folder to dump.")
 	}
-	defer in.Close()
-	out, err := os.Create(dst)
-	if err != nil {
-		return
+	file_to_pars := os.Args[1]
+	out_dir = path.Join("..", os.Args[2])
+	return file_to_pars, out_dir
+}
+
+func cpDir(src string, dst string) error {
+	var err error
+	var fds []os.FileInfo
+	var srcinfo os.FileInfo
+
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
 	}
-	defer func() {
-		cerr := out.Close()
-		if err == nil {
-			err = cerr
+
+	if err = os.MkdirAll(dst, srcinfo.Mode()); err != nil {
+		return err
+	}
+
+	if fds, err = ioutil.ReadDir(src); err != nil {
+		return err
+	}
+	for _, fd := range fds {
+		srcfp := path.Join(src, fd.Name())
+		dstfp := path.Join(dst, fd.Name())
+
+		if fd.IsDir() {
+			if err = cpDir(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			if err = cpFile(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
 		}
-	}()
-	if _, err = io.Copy(out, in); err != nil {
-		return
 	}
-	err = out.Sync()
-	return
+	return nil
+}
+
+func cpFile(src, dst string) error {
+	var err error
+	var srcfd *os.File
+	var dstfd *os.File
+	var srcinfo os.FileInfo
+
+	if srcfd, err = os.Open(src); err != nil {
+		return err
+	}
+	defer srcfd.Close()
+
+	if dstfd, err = os.Create(dst); err != nil {
+		return err
+	}
+	defer dstfd.Close()
+
+	if _, err = io.Copy(dstfd, srcfd); err != nil {
+		return err
+	}
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+	}
+	return os.Chmod(dst, srcinfo.Mode())
 }
